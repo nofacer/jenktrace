ec197bc770e8e0118caf59974d9de6ef85c5245c
PosiNorth <nofacer@outlook.com>
2026-06-01 18:26:39 +0800

    v1.0.2 (#4)
    * update
    
    * update

--- src/mainview/lib/app-rpc.ts
+++ src/mainview/lib/app-rpc.ts
@@ -2,7 +2,10 @@ import { Electroview } from "electrobun/view";
 
 import type { AppRPCSchema } from "../../shared/rpc";
 
+const RPC_REQUEST_TIMEOUT_MS = 10_000;
+
 export const appRpc = Electroview.defineRPC<AppRPCSchema>({
+	maxRequestTime: RPC_REQUEST_TIMEOUT_MS,
 	handlers: {},
 });
 
